f0b893ef7208ab350d8c930876f070893b470173
FY Kuo <teresa99339@gmail.com>
2022-04-18 14:10:30 +0800

    string 函式

---
+++ index.php
@@ -0,0 +1,10 @@
+<h1>PHP字串課程</h1>
+
+<h3>常用字串</h3>
+<h2>substr() / mb_substr()</h2>
+<ul>
+    <li>substr - 用於英文系統單字字集</li>
+    <li>mb_substr - 用多語系統雙字元字集</li>
+</ul>
+
+<h4>字串 - 今天是星期一,是個blue monday</h4>
